31469489bd6d77a1be5b5e9951ce633f566a212f
Knchin <313461756+Knchin@users.noreply.github.com>
2026-08-19 00:25:05 +0000

    fix: correct webApp JS source set path, task name, and plugin version
    - Add org.jetbrains.kotlin.js version 1.8.22 to settings.gradle.kts
    - Move source from src/main/ to src/jsMain/ (matches jsMain source set)
    - Remove unused kotlinx.coroutines/serialization imports from Main.kt
    - Use jsBrowserProductionWebpack task (correct for Kotlin 1.8.22)
    - Search build/dist/js/productionExecutable for output

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.multiplatform") version "1.8.22"
         id("org.jetbrains.kotlin.android") version "1.8.22"
         id("org.jetbrains.kotlin.jvm") version "1.8.22"
+        id("org.jetbrains.kotlin.js") version "1.8.22"
     }
 }
 
---
+++ webApp/src/jsMain/kotlin/france/fuel/station/web/Main.kt
@@ -0,0 +1,5 @@
+package france.fuel.station.web
+
+fun main() {
+    println("France Fuel Station Web Application loaded")
+}
